a375562732ffa3bdfedf92ba0f0684e79e844362
kirillOff <kirilloffkostya@gmail.com>
2022-06-27 17:09:04 +0300

    add logic for segue between screens

--- LoginScreenUIkit/LoginViewController.swift
+++ LoginScreenUIkit/LoginViewController.swift
@@ -27,9 +27,45 @@ class LoginViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        <#code#>
+        guard let welcomVC = segue.destination as? WelcomViewController else { return }
+        welcomVC.nickname = userNameTextField.text
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
     }
 
+    
+    @IBAction func logInPressed() {
+        if userNameTextField.text != "Alex" ||
+            passwordTextField.text != "123" {
+            showAlertWith(title: "Ooops!", hint: "Not correct login or password", errorData: true)
+        }
+    }
+    
+    @IBAction func forgotPressedButtons(_ sender: UIButton) {
+        if sender.tag == 1 {
+            showAlertWith(title: "Don't forget! There is your user name:", hint: "Alex")
+        } else {
+            showAlertWith(title: "Don't forget! There is your password:", hint: "123")
+        }
+    }
+    
+}
 
+extension LoginViewController {
+    private func showAlertWith(title: String, hint: String, errorData: Bool = false) {
+        let alertController = UIAlertController(title: title, message: hint, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default) { UIAlertAction in
+            if errorData {
+                self.passwordTextField.text = ""
+            }
+        }
+        
+        alertController.addAction(alertAction)
+        present(alertController, animated: true)
+        
+    }
 }
 
--- LoginScreenUIkit/WelcomViewController.swift
+++ LoginScreenUIkit/WelcomViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class WelcomViewController: UIViewController {
+    
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    var nickname: String!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        greetingLabel.text = "\(greetingLabel.text ?? "") \(nickname ?? "")"
 
     }
 
